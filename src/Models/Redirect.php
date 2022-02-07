82ecd3fd7c9db93fd81fdc66a1d35886ca7edb80
Robin Mulder <robin@justbetter.nl>
2022-02-07 13:23:37 +0100

    Define primary_key for Redirect Model and add ForCurrentStoreScope scope to Redirect Model

--- src/Models/Redirect.php
+++ src/Models/Redirect.php
@@ -4,13 +4,25 @@ namespace Rapidez\MirasvitAdvancedSeoSuite\Models;
 
 use Rapidez\Core\Models\Model;
 use Rapidez\Core\Models\Scopes\IsActiveScope;
+use Rapidez\Core\Models\Scopes\ForCurrentStoreScope;
 
 class Redirect extends Model
 {
+    /**
+     * The primary key for the model.
+     * @var string
+     */
+    protected $primaryKey = 'redirect_id';
+
+    /**
+     * The table associated with the model.
+     * @var string
+     */
     protected $table = 'mst_seo_redirect';
 
     protected static function booted()
     {
         static::addGlobalScope(new IsActiveScope());
+        static::addGlobalScope(new ForCurrentStoreScope());
     }
 }
