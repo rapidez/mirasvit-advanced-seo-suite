44b7cf7b72c4e2ec34c36c42297a845ec1e93e42
Roy Duineveld <royduineveld@gmail.com>
2022-02-07 13:39:22 +0100

    Update Redirect.php

--- src/Models/Redirect.php
+++ src/Models/Redirect.php
@@ -8,17 +8,9 @@ use Rapidez\Core\Models\Scopes\ForCurrentStoreScope;
 
 class Redirect extends Model
 {
-    /**
-     * The primary key for the model.
-     * @var string
-     */
-    protected $primaryKey = 'redirect_id';
-
-    /**
-     * The table associated with the model.
-     * @var string
-     */
     protected $table = 'mst_seo_redirect';
+    
+    protected $primaryKey = 'redirect_id';
 
     protected static function booted()
     {
