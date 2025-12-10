f7fd5bb946d9feeaea5d38e29abf88684ec30312
Claudio Ferraro <claudio@justbetter.nl>
2025-12-10 11:50:20 +0100

    Implement avoid duplicate URLs in cross-links option

--- src/Models/Autolink.php
+++ src/Models/Autolink.php
@@ -10,6 +10,10 @@ class Autolink extends Model
 {
     protected $table = 'mst_seoautolink_link';
 
+    protected const CONFIG_LINKS_LIMIT = 'seoautolink/autolink/links_limit_per_page';
+
+    protected const CONFIG_STOP_KEYWORD_PROCESSING = 'seoautolink/autolink/stop_keyword_processing';
+
     protected static function booted()
     {
         static::addGlobalScope(new IsActiveScope());
@@ -17,20 +21,32 @@ class Autolink extends Model
 
     public static function replace(string $text): string
     {
-        $limit = (int) Rapidez::config('seoautolink/autolink/links_limit_per_page', -1);
+        $limit = (int) Rapidez::config(self::CONFIG_LINKS_LIMIT, -1);
+        $avoidDuplicateUrls = (bool) Rapidez::config(self::CONFIG_STOP_KEYWORD_PROCESSING, 0);
         $counter = 0;
+        $usedUrls = [];
 
-        self::all()->each(function ($autolink) use (&$text, &$counter, $limit) {
-            $url = url($autolink->url);
-            $link = " <a href=\"$url\" target=\"$autolink->url_target\" title=\"$autolink->url_title\">$autolink->keyword</a> ";
-
+        self::all()->each(function ($autolink) use (&$text, &$counter, &$usedUrls, $limit, $avoidDuplicateUrls) {
             if ($limit > -1 && $counter >= $limit) {
                 return false;
             }
 
+            $url = url($autolink->url);
+
+            if ($avoidDuplicateUrls && in_array($url, $usedUrls)) {
+                return;
+            }
+
             $remaining = $limit > -1 ? max($limit - $counter, 0) : -1;
+            $link = " <a href=\"$url\" target=\"$autolink->url_target\" title=\"$autolink->url_title\">$autolink->keyword</a> ";
+
             $text = preg_replace('/ '.$autolink->keyword.' (?!([^<]+)?>)/i', $link, $text, $remaining, $count);
+
             $counter += $count;
+
+            if ($count > 0) {
+                $usedUrls[] = $url;
+            }
         });
 
         return $text;
