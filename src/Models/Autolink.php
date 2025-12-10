d33bd22567e348972ea8413765ed6437a27671d1
Claudio Ferraro <claudio@justbetter.nl>
2025-12-10 12:49:13 +0100

    Add autolink number of substitutions support

--- src/Models/Autolink.php
+++ src/Models/Autolink.php
@@ -22,22 +22,21 @@ class Autolink extends Model
     public static function replace(string $text): string
     {
         $limit = (int) Rapidez::config(self::CONFIG_LINKS_LIMIT, -1);
-        $avoidDuplicateUrls = (bool) Rapidez::config(self::CONFIG_STOP_KEYWORD_PROCESSING, 0);
         $counter = 0;
         $usedUrls = [];
 
-        self::all()->each(function ($autolink) use (&$text, &$counter, &$usedUrls, $limit, $avoidDuplicateUrls) {
-            if ($limit > -1 && $counter >= $limit) {
+        self::all()->each(function ($autolink) use (&$text, &$counter, &$usedUrls, $limit) {
+            if (self::shouldStopProcessing($counter, $limit)) {
                 return false;
             }
 
             $url = url($autolink->url);
 
-            if ($avoidDuplicateUrls && in_array($url, $usedUrls)) {
+            if (self::shouldSkipDuplicateUrl($url, $usedUrls)) {
                 return;
             }
 
-            $remaining = $limit > -1 ? max($limit - $counter, 0) : -1;
+            $remaining = self::getRemainingReplacements($autolink, $limit, $counter);
             $link = " <a href=\"$url\" target=\"$autolink->url_target\" title=\"$autolink->url_title\">$autolink->keyword</a> ";
 
             $text = preg_replace('/ '.$autolink->keyword.' (?!([^<]+)?>)/i', $link, $text, $remaining, $count);
@@ -50,5 +49,34 @@ class Autolink extends Model
         });
 
         return $text;
+    }
+
+    protected static function shouldStopProcessing(int $counter, int $limit): bool
+    {
+        return $limit > -1 && $counter >= $limit;
+    }
+
+    protected static function shouldSkipDuplicateUrl(string $url, array $usedUrls): bool
+    {
+        return (bool) Rapidez::config(self::CONFIG_STOP_KEYWORD_PROCESSING, false) && in_array($url, $usedUrls);
+    }
+
+    /**
+     * Determine the maximum number of replacements allowed for this autolink.
+     * Respects both the global page limit and the keyword-specific limit by returning the lowest of the two.
+     * Returns -1 for unlimited replacements.
+     */
+    protected static function getRemainingReplacements(Autolink $autolink, int $globalLimit, int $counter): int
+    {
+        $keywordLimit = (int) ($autolink->max_replacements ?: -1);
+        $globalRemaining = $globalLimit > -1 ? max($globalLimit - $counter, 0) : -1;
+
+        return match (true) {
+            $globalRemaining === 0 => 0,
+            $globalRemaining > -1 && $keywordLimit > -1 => min($globalRemaining, $keywordLimit),
+            $globalRemaining > -1 => $globalRemaining,
+            $keywordLimit > -1 => $keywordLimit,
+            default => -1,
+        };
     }
 }
